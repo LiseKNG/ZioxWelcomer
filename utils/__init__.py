a046767c40230de233966ed79b713275def80f08
Winter.Vfz <lisemdarenne@gmail.com>
2026-07-30 10:32:33 +0100

    Create __init__.py for utility functions
    Add utility functions for ZioxWelcomer in __init__.py

---
+++ utils/__init__.py
@@ -0,0 +1,12 @@
+"""
+utils/__init__.py
+
+Fonctions utilitaires de ZioxWelcomer.
+"""
+
+from .logger import logger
+from .permissions import is_admin
+from .formatter import (
+    format_welcome,
+    format_goodbye,
+)
